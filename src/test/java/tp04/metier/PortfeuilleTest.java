aedf9fc3a54f935e05cdeff4e7055f8add339ad7
XiaoYan YIN <115007671+Xiaoyan-YIN@users.noreply.github.com>
2025-03-17 16:40:59 +0100

    Update PortfeuilleTest.java

--- src/test/java/tp04/metier/PortfeuilleTest.java
+++ src/test/java/tp04/metier/PortfeuilleTest.java
@@ -1,3 +1,13 @@
 public class PortfeuilleTest {
-
+@Test
+void testAcheterActionNegative() {
+    Client c = new client("testnom","testprenom");
+    portfeuille p = new portfeuille();
+    Action action = new Action("TestA");
+    p.acheter(action, 0);
+    assertNull(p.mapLignes.get(action), "le qte doit plus de 0!");
+    p.acheter(action, -5);
+    assertNull(p.mapLignes.get(action), "le qte doit plus de 0!");
+    c.AcheterPortefueille(p);
+}
 }
