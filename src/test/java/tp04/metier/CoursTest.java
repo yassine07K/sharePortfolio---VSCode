85b0b8f53fd74e56cf5c97db16c8a81aeacf1a49
XiaoYan YIN <115007671+Xiaoyan-YIN@users.noreply.github.com>
2025-03-23 15:28:58 +0100

    Update CoursTest.java

--- src/test/java/tp04/metier/CoursTest.java
+++ src/test/java/tp04/metier/CoursTest.java
@@ -6,14 +6,5 @@ import java.util.HashMap;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 public class CoursTest {
-    @Test
-    void testCours() {
-        Jour j = new Jour(1);
-        ActionSimple action = new ActionSimple("ActionX");
-        Cours cours = new Cours(j, 25.0f);
-        action.enregistrerCours(cours);
-    
-        Assertions.assertEquals(25.0f, action.valeur(j));
-    }
-    
+   
 }
