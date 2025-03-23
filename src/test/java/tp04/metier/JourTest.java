90a638ac5ae935cbfd4cb1008c61ce73629b6a93
XiaoYan YIN <115007671+Xiaoyan-YIN@users.noreply.github.com>
2025-03-23 15:29:15 +0100

    Update JourTest.java

--- src/test/java/tp04/metier/JourTest.java
+++ src/test/java/tp04/metier/JourTest.java
@@ -6,12 +6,6 @@ import java.util.HashMap;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 public class JourTest {
-@Test
-void testJourEqualsAndHashCode() {
-    Jour j1 = new Jour(2);
-    Jour j2 = new Jour(2);
-    Assertions.assertEquals(j1, j2);
-    Assertions.assertEquals(j1.hashCode(), j2.hashCode());
-}
+
 
 }
