5b29d760d1e1ef9a69678877feca959aec00c357
zxc2323 <lvshihao0525@gmail.com>
2025-03-26 10:08:38 +0100

    Tester Fonction Client version 1

--- src/test/java/tp04/metier/ClientTest.java
+++ src/test/java/tp04/metier/ClientTest.java
@@ -27,9 +27,39 @@ public class ClientTest {
         final Client client = new Client("nom", "prenom");
         final Action action = new ActionImpl("Action1");
         
+        ActionSimple actions1 = new ActionSimple("Action2");
+        ActionSimple actions2 = new ActionSimple("Action3");
+        ActionSimple actions3 = new ActionSimple("Action4");
+        ActionSimple actions4 = new ActionSimple("Action5");
+
+        Jour j = new Jour(1, 1);
+        Jour j1 = new Jour(2,2);
+        Jour j2 = new Jour(2, 3);
+        actions1.enrgCours(j, 10);
+        actions1.enrgCours(j1, 20);
+
+        actions2.enrgCours(j2, 22);
+
+        actions3.enrgCours(j, 30);
+        actions3.enrgCours(j2, 40);
+
+        actions4.enrgCours(j2, 1);
+
+        ActionComposee AC = new ActionComposee("ActionComposee1");
+        AC.enrgComposition(actions4, 30);
+        AC.enrgComposition(actions1, 70);
+
+
         client.creerPortefeuille("monPorte");
+
         assertEquals(true,client.creerPortefeuille("monPorte"), "une erreur est survenue lors de la création du portefeuille");
         assertEquals(true, client.acheter("monPorte",action, 10), "L'action achetée doit être la même que celle passée en paramètre");
+
+        assertEquals(true,client.acheter("monPorte", AC, 10), "L'action achetée doit être la même que celle passée en paramètre");
+        assertEquals(true,client.acheter("monPorte", actions1, 10), "L'action achetée doit être la même que celle passée en paramètre");
+        assertEquals(true,client.acheter("monPorte", actions4, 10), "L'action achetée doit être la même que celle passée en paramètre");
+
+        assertEquals(true, client.removePortefeuille("monPorte"), "Le portefeuille doit être supprimé");
 
     }
 
